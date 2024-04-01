e10c44ae37b7a0eb5270c2a6b47973740bdddf07
Amr <amro.amor1234@gmail.com>
2024-04-01 13:40:12 +0200

    Bind() method

--- 06 - More on Functions/Rock,Paper,Scissors/app.js
+++ 06 - More on Functions/Rock,Paper,Scissors/app.js
@@ -80,3 +80,40 @@ const multiply = (a, b, cb) => {
 multiply(5,
     5,
     printToLog);
+//Working with bind method
+
+const combine = (resultHandler, operation, ...numbers) => {
+    const validateNumber = (number) => {
+        return isNaN(number) ? 0 : number;
+    }
+    let sum = 0;
+    for (const num of numbers) {
+        if (operation === 'ADD') {
+            sum += validateNumber(num);
+        } else {
+            sum -= validateNumber(num);
+        }
+    }
+    resultHandler(sum);
+}
+const showResult = (messageText, result) => {
+    alert(messageText + ' ' + result);
+}
+
+combine(showResult.bind(this,
+        'The result after adding all numbers is '),
+    "ADD",
+    1,
+    3,
+    4,
+    2,
+    5);
+combine(showResult.bind(this,
+        'The result after subtracting all numbers is '),
+    "SUBTRACT",
+    100,
+    50,
+    20,
+    10);
+
+
