caf0c1aba2cd504a9b308181fe036f525136ce50
Amr <amro.amor1234@gmail.com>
2024-04-01 23:10:19 +0200

    Understand a bit now

---
+++ 07 - Working with the DOM (Browser HTML Code) in JavaScript/Learining The DOM/app.js
@@ -0,0 +1,18 @@
+const section = document.getElementById("section-id");
+const button = document.getElementById('Btn-id');
+
+
+section.className='red-bg'
+button.addEventListener('click',()=>{
+    // one way
+    // if(section.className === 'red-bg visible')
+    // {
+    //     section.className = 'red-bg invisible';
+    // }else{
+    //     section.className = 'red-bg visible';
+    // }
+
+    //or another
+    section.classList.toggle('invisible');
+
+})
