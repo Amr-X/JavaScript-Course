23e359454fc962c9fd59601bed71fb14b82362c3
Amr <amro.amor1234@gmail.com>
2024-04-05 13:34:24 +0200

    Sets

---
+++ 08 - More on Arrays & Iterables/Arrays & Iterables/Sets.js
@@ -0,0 +1,16 @@
+//sets (just array but no duplicates)
+//cant use the array method!
+// const set2 = new Set([2,4,4,3,5]);
+// console.log(set2); // [2,4,3,5]
+// console.log(set2.add(7)) //[2,4,3,5,7]
+// if(set2.has(2)){ //true it has 2
+//     set2.delete(2); //delete 2
+// }
+// console.log(set2); //[4,3,5,7]
+// for (const entry of set2.entries()){ //every entry is now array with 2 same element
+//     // [4,4] then [3,3] so on
+//     console.log(entry);
+//      //[4,4] //[3,3] //[5,5] //[7,7]
+// }
+
+
