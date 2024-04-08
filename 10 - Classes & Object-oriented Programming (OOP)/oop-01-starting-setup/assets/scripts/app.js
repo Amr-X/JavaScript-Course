e0d18caca613b9d607532e64cd281d277c65fff0
Amr <amro.amor1234@gmail.com>
2024-04-08 17:44:49 +0200

    Constructor

--- 10 - Classes & Object-oriented Programming (OOP)/oop-01-starting-setup/assets/scripts/app.js
+++ 10 - Classes & Object-oriented Programming (OOP)/oop-01-starting-setup/assets/scripts/app.js
@@ -1,8 +1,14 @@
 class Product { //Capital case// This is the default object blueprint
     title ='DEFAULT'; //WATCH OUT! THAT IS THE SYNTAX
-    imgUrl;
+    imgUrl; //class Field
     description;
     price;
+    constructor(title,img,des,price) {
+        this.title=title; //class property
+        this.imgUrl=img;
+        this.description=des;
+        this.price=price
+    }
 }
 //how to create one?
 //console.log(new Product()) //a new object have the default value
@@ -10,11 +16,15 @@ class Product { //Capital case// This is the default object blueprint
 
 
 const productList ={
-    products:[
-        {title: 'A Pillow',imgUrl:'https://m.media-amazon.com/images/I/51P+okcVHYL.jpg',price:19.99,description:'A soft Pillow'}
-        ,{title: 'A Carpet',imgUrl:'https://m.media-amazon.com/images/I/81GyZXnRB5L._AC_UF894,1000_QL80_.jpg',price:89.99,description:'A Carpet'}
-        ] //What if I need to add more products here in this array? I mean like a million products.
+    // products:[
+    //     {title: 'A Pillow',imgUrl:'https://m.media-amazon.com/images/I/51P+okcVHYL.jpg',price:19.99,description:'A soft Pillow'}
+    //     ,{title: 'A Carpet',imgUrl:'https://m.media-amazon.com/images/I/81GyZXnRB5L._AC_UF894,1000_QL80_.jpg',price:89.99,description:'A Carpet'}
+    //     ] //What if I need to add more products here in this array? I mean like a million products.
         //Here comes OOP in handy.
+    products:[
+        new Product('A Pillow','https://m.media-amazon.com/images/I/51P+okcVHYL.jpg',19.99,'A soft Pillow'),
+        new Product('A Carpet','https://m.media-amazon.com/images/I/81GyZXnRB5L._AC_UF894,1000_QL80_.jpg',89.99,'A Carpet')
+    ]
     ,render(){
         const renderHook =document.getElementById('app');
         const productList = document.createElement('ul');
